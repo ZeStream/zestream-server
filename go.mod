e1f41b8e60c451abe593d558388b38b83dde5426
sreehari2003 <sreeharivijaya2003@gmail.com>
2023-01-01 20:20:56 +0530

    fix: conflicts

--- go.mod
+++ go.mod
@@ -12,6 +12,20 @@ require (
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/swaggo/swag v1.8.1 // indirect
+	golang.org/x/tools v0.1.12 // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
@@ -28,6 +42,8 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/swaggo/files v1.0.0
+	github.com/swaggo/gin-swagger v1.5.3
 	github.com/u2takey/go-utils v0.3.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 	"os"
 	"zestream-server/configs"
 	"zestream-server/constants"
+	"zestream-server/docs"
 	"zestream-server/routes"
 	"zestream-server/service"
 	"zestream-server/utils"
+
+	swaggerFiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
 func dev() {
@@ -24,6 +28,14 @@ func main() {
 
 	port := os.Getenv(constants.PORT)
 
+	// Swagger 2.0 Meta Information
+	docs.SwaggerInfo.Title = "ZeStream - An adaptive video streaming server"
+	docs.SwaggerInfo.Description = "ZeStream is the backend service which you can self-deploy, and use its API to process the video and store it on a storage bucket like AWS S3/Google Cloud/Azure...."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = ":" + port
+	docs.SwaggerInfo.BasePath = "/api/v1"
+	docs.SwaggerInfo.Schemes = []string{"https"}
+
 	kafkaURI := os.Getenv("KAFKA_URI")
 	if kafkaURI == "" {
 		log.Fatal("Error: KAFKA_URI environment variable not set")
@@ -33,6 +45,7 @@ func main() {
 		port = constants.DEFAULT_PORT
 	}
 
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	err := http.ListenAndServe(port, nil)
 
 	if err != nil {
