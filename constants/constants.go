e7bf398709b001dc0dd39808a6b090555e86482f
Vivek Khatri <vivek@esamudaay.com>
2023-01-03 22:42:59 +0530

    Add timeouts for server

--- constants/constants.go
+++ constants/constants.go
@@ -14,3 +14,6 @@ const S3_REGION = "us-east-1"
 const AWS_ENDPOINT = "http://localhost:4566"
 const PRESIGNED_URL_EXPIRATION = 60 * time.Minute
 const OUTPUT_FILE_PATH_PREFIX = "output"
+const READ_TIMEOUT = 1 * time.Second
+const WRITE_TIMEOUT = 1 * time.Second
+const IDLE_TIMEOUT = 30 * time.Second
