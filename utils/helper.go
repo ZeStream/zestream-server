24f0a7ed9ab2342bcd8468dc9518fbbc6a1adabc
Vivek Khatri <vivek@esamudaay.com>
2023-01-04 22:14:47 +0530

    Change the hash generation algo in seed phase

--- utils/helper.go
+++ utils/helper.go
@@ -1,14 +1,30 @@
 package utils
 
 import (
+	"encoding/binary"
+	"fmt"
+	"hash/crc64"
 	"math/rand"
 	"strconv"
-	"time"
 )
 
 func VideoIDGen(fileExtension string) string {
 	// Seed the random number generator
-	rand.Seed(time.Now().UnixNano())
+	hash := crc64.New(crc64.MakeTable(crc64.ECMA))
+
+	// Write the string to the hash
+	_, err := hash.Write([]byte("zestream"))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Calculate the hash
+	sum := hash.Sum(nil)
+
+	// Convert the hash to an int64 value
+	var i int64
+	i = int64(binary.LittleEndian.Uint64(sum))
+	rand.Seed(i)
 
 	// Generate a 8 digit random number
 	randomNumber := rand.Intn(100000000) + 10000000
