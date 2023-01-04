6f3e0104e0418abcf56dcb328a367d505858c657
Vivek Khatri <vivek@esamudaay.com>
2023-01-04 22:36:19 +0530

    remove crc64 hash + use crypto/rand to generate random num

--- utils/helper.go
+++ utils/helper.go
@@ -1,32 +1,23 @@
 package utils
 
 import (
+	"crypto/rand"
 	"encoding/binary"
-	"fmt"
-	"hash/crc64"
-	"math/rand"
+	math_rand "math/rand"
 	"strconv"
 )
 
 func VideoIDGen(fileExtension string) string {
-	// Seed the random number generator
-	hash := crc64.New(crc64.MakeTable(crc64.ECMA))
-
-	// Write the string to the hash
-	_, err := hash.Write([]byte("zestream"))
-	if err != nil {
-		fmt.Println(err)
+	var b [8]byte
+	if _, err := rand.Read(b[:]); err != nil {
+		return err.Error()
 	}
 
-	// Calculate the hash
-	sum := hash.Sum(nil)
-
-	// Convert the hash to an int64 value
-	var i int64 = int64(binary.LittleEndian.Uint64(sum))
-	rand.Seed(i)
+	var i int64 = int64(binary.LittleEndian.Uint64(b[:]))
+	math_rand.Seed(i)
 
 	// Generate a 8 digit random number
-	randomNumber := rand.Intn(100000000) + 10000000
+	randomNumber := math_rand.Intn(100000000) + 10000000
 
 	// VideoID = (8-digit random number) + (file Name)
 	return strconv.Itoa(randomNumber) + fileExtension
