eff6722ad6e8b75b4a11e6413af838fc528069bd
Shofiya2003 <shofiyabootwala@gmail.com>
2023-01-10 05:33:37 +0530

    removed dev function from main.go to resolve linting errors

--- main.go
+++ main.go
@@ -8,13 +8,7 @@ import (
 	"zestream-server/configs"
 	"zestream-server/constants"
 	"zestream-server/routes"
-	"zestream-server/service"
 )
-
-func dev() {
-	// utils.Fetch("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4", "Test.mp4")
-	service.GenerateDash("Test.mp4", constants.WaterMark{FileName: "TestWatermark.png", Dimension: map[string]int{"x": 64, "y": -1}, Position: map[string]int{"x": 10, "y": 10}}, false)
-}
 
 func main() {
 	configs.LoadEnv()
