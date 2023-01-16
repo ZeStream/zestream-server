91a4c8ba90166079f3aa1d27c8dd74fef5f69da3
Abhishek Raj <abhishekraj272@gmail.com>
2023-01-16 22:44:01 +0530

    feat: added pre sign url support for gcp and azure

--- controllers/upload.go
+++ controllers/upload.go
@@ -3,17 +3,12 @@ package controllers
 import (
 	"net/http"
 	"path/filepath"
-	"zestream-server/configs"
-	"zestream-server/constants"
 	"zestream-server/utils"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/gin-gonic/gin"
 )
 
-func GeneratePresignedAWSURL(c *gin.Context, s3Client s3iface.S3API) {
+func GeneratePresignedURL(c *gin.Context) {
 
 	// Obtain the file name and extension from query params
 	fileName := c.Query("fileName")
@@ -28,18 +23,7 @@ func GeneratePresignedAWSURL(c *gin.Context, s3Client s3iface.S3API) {
 	videoID := utils.VideoIDGen(extension)
 
 	// Create a PutObjectRequest with the necessary parameters
-	req, _ := s3Client.PutObjectRequest(&s3.PutObjectInput{
-		Bucket: aws.String(configs.EnvVar[configs.AWS_S3_BUCKET_NAME]),
-		Key:    aws.String(videoID),
-	})
+	preSignedURL := utils.GetPreSignedURL(videoID)
 
-	//  Sign the request and generate a presigned URL
-	urlStr, err := req.Presign(constants.PRESIGNED_URL_EXPIRATION)
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error generating presigned URL", "details": err.Error()})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"preSignedURL": urlStr, "videoID": videoID})
+	c.JSON(http.StatusOK, gin.H{"preSignedURL": preSignedURL, "videoID": videoID})
 }
